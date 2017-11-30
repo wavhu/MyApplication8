c4e0d46045f6abf194c985ffb5f8b04340c659c3
wavhu <mmboneni11@gmail.com>
2017-11-30 09:51:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/codetribe/myapplication/slideshow.java
@@ -0,0 +1,13 @@
+package com.example.codetribe.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class slideshow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slideshow);
+    }
+}
